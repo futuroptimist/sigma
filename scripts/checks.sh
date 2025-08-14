a40e46b17efb82669de7a1edcad4da4e225b1bfb
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2025-08-13 21:13:30 -0700

    🛠️ : – use python3 in checks script

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python -m flake8 . --exclude=.venv
-python -m isort --check-only . --skip .venv
-python -m black --check . --exclude ".venv/"
+python3 -m flake8 . --exclude=.venv
+python3 -m isort --check-only . --skip .venv
+python3 -m black --check . --exclude ".venv/"
 
 pytest -q
